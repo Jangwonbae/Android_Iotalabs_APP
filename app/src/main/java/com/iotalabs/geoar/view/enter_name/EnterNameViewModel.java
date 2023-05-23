1a8d2368c541df6b041bfc92c7f844235eb10288
Jangwonbae <84276491+Jangwonbae@users.noreply.github.com>
2023-05-24 00:18:44 +0900

    SingleLiveEvent 클래스생성하여 중복 호출 수정

--- app/src/main/java/com/iotalabs/geoar/view/enter_name/EnterNameViewModel.java
+++ app/src/main/java/com/iotalabs/geoar/view/enter_name/EnterNameViewModel.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import com.iotalabs.geoar.data.PersonName;
+import com.iotalabs.geoar.data.SingleLiveEvent;
 
 public class EnterNameViewModel extends ViewModel {
     private PersonName personName;
-    MutableLiveData<String> enterNameLiveData = new MutableLiveData<>();
-
+    //SingleLiveEvent 사용해서 Event가 한번만 발동하도록
+    SingleLiveEvent<String> enterNameLiveData = new SingleLiveEvent<>();
+    //AAC ViewModel은 Activity의 모든 생명주기에 함께하기 때문에 LiveData에 있는 값이 Actvity가 재생성 될때 LiveData의 상태가 Active로 바뀌면서 한번 더 중복 호출됨
     public void enterName(String name, Context context){//이름을 입력했을 때 실행
 
         personName = PersonName.getInstance(context);
@@ -21,3 +23,4 @@ public class EnterNameViewModel extends ViewModel {
         }
     }
 }
+
