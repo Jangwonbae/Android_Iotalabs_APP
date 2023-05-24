d33e3e57d7f53705c778ab8ef741d710cc09dfbf
Jangwonbae <84276491+Jangwonbae@users.noreply.github.com>
2023-05-25 00:00:48 +0900

    MainActivity DataBinding

--- app/src/main/java/com/iotalabs/geoar/view/main/MainActivity.java
+++ app/src/main/java/com/iotalabs/geoar/view/main/MainActivity.java
@@ -2,175 +2,163 @@ package com.iotalabs.geoar.view.main;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.example.lotalabsappui.R;
+import com.example.lotalabsappui.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.iotalabs.geoar.util.location.BackgroundLocationUpdateService;
 import com.iotalabs.geoar.view.create_qr_code.CreateQR_codeActivity;
 import com.iotalabs.geoar.view.read_qr_code.ReadQR_codeActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
-    private BottomNavigationView bottomNavigationView; // 바텀 네비게이션 뷰
-    private FragmentManager fm;
-    private FragmentTransaction ft;
-    private MapFragment frag1;
-    private ListFragment frag2;
-    private SettingFragment frag3;
-    private Animation fab_open, fab_close;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private MapFragment mapFragment;
+    private ListFragment listFragment;
+    private SettingFragment settingFragment;
+    private Animation ft_btn_open, ft_btn_close;
     private Boolean isFabOpen = false;
-    private FrameLayout fab_layout;
-    private ExtendedFloatingActionButton fab1, fab2;
-    private FloatingActionButton fab;
     private long backKeyPressedTime = 0; //뒤로가기 버튼 눌렀던 시간 저장
     private Toast toast;//첫번째 뒤로가기 버튼을 누를때 표시하는 변수
-    private final String TAG = "MainActivity";
-
-
-    public Context context;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //데이터 바인딩
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setActivity(this);
+        binding.frameLayoutMainWhole.bringToFront();//버튼이 있는 fragment가 제일 앞으로 오도록 설정
 
+        //Fragment 객체생성
+        mapFragment = new MapFragment();
+        listFragment = new ListFragment();
+        settingFragment = new SettingFragment();
 
-        context = this.getApplicationContext();
-
+        //백그라운드 위치서비스
         startService(new Intent(this, BackgroundLocationUpdateService.class));
 
+        //애니메시션 객체생성
+        ft_btn_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_ft_btn_open);
+        ft_btn_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_ft_btn_close);
 
-        //프래그먼트 설정
-        fab_layout = findViewById(R.id.fab_layout);
-        fab_layout.bringToFront();
-        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
-        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
-
-        fab = (FloatingActionButton) findViewById(R.id.fab);//메인
-        fab1 = (ExtendedFloatingActionButton) findViewById(R.id.fab1);//qr생성
-        fab2 = (ExtendedFloatingActionButton) findViewById(R.id.fab2);//qr스캔
-
-        fab.setOnClickListener(this);
-        fab1.setOnClickListener(this);
-        fab2.setOnClickListener(this);
-        fab.bringToFront() ;
-
-        bottomNavigationView = findViewById(R.id.bottomNavi);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
-        {
+        //네비게이션바 클릭리스너
+        binding.navigationBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
-            {
-                switch (menuItem.getItemId())
-                {
-                    case R.id.action_map:
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_map://MapFragment
                         setFrag(0);
                         break;
-                    case R.id.action_list:
+                    case R.id.action_list://ListFragment
                         setFrag(1);
                         break;
-                    case R.id.action_set:
+                    case R.id.action_set://SettingFragment
                         setFrag(2);
                         break;
                 }
+
                 return true;
             }
         });
-        frag1=new MapFragment();
-        frag2=new ListFragment();
-        frag3=new SettingFragment();
         setFrag(0); // 첫 프래그먼트 화면 지정
-
-    }
-
-
-    public void onClick(View v) {
-        int id = v.getId();
-        switch (id) {
-            case R.id.fab://메인버튼
+        //플로팅 버튼 클릭메소드
+        binding.ftBtnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 anim();
-                break;
-            case R.id.fab1://qr생성
+            }
+        });
+        binding.ftBtnCreateQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 anim();
                 Intent intent = new Intent(MainActivity.this, CreateQR_codeActivity.class);
                 startActivity(intent);
-                break;
-            case R.id.fab2://qr스캔
+            }
+        });
+        binding.ftBtnReadQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 anim();
                 Intent intent2 = new Intent(MainActivity.this, ReadQR_codeActivity.class);
                 startActivity(intent2);
-                break;
-        }
+            }
+        });
     }
-    public void service_start(){
-        startService(new Intent(this, BackgroundLocationUpdateService.class));
-    }
-    //백그라운드 서비스 종료
-    public void service_stop() {
-        stopService(new Intent(this, BackgroundLocationUpdateService.class));
-    }
+
 
     public void anim() {
-
         if (isFabOpen) {
-            fab1.startAnimation(fab_close);
-            fab2.startAnimation(fab_close);
-            fab1.setClickable(false);
-            fab2.setClickable(false);
+            binding.ftBtnCreateQR.startAnimation(ft_btn_close);
+            binding.ftBtnReadQR.startAnimation(ft_btn_close);
+            binding.ftBtnCreateQR.setClickable(false);
+            binding.ftBtnReadQR.setClickable(false);
             isFabOpen = false;
         } else {
-            fab1.startAnimation(fab_open);
-            fab2.startAnimation(fab_open);
-            fab1.setClickable(true);
-            fab2.setClickable(true);
+            binding.ftBtnCreateQR.startAnimation(ft_btn_open);
+            binding.ftBtnReadQR.startAnimation(ft_btn_open);
+            binding.ftBtnCreateQR.setClickable(true);
+            binding.ftBtnReadQR.setClickable(true);
             isFabOpen = true;
         }
     }
 
     // 프레그먼트 교체
-    public void setFrag(int n)
-    {
-        fm = getSupportFragmentManager();
-        ft= fm.beginTransaction();
-        switch (n)
-        {
+    public void setFrag(int n) {
+
+        // FragmentManager를 통해서 FragmentTransaction 획득하기
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        //setReorderingAllowed(true)는 transaction과 관련된 프래그먼트의 상태 변경을 최적화하여 애니메이션과 전환이 올바르게 작동하도록 함
+        fragmentTransaction.setReorderingAllowed(true);
+
+        switch (n) {
             case 0:
-                ft.replace(R.id.Main_Frame,frag1);
-                ft.commit();
+                //replace 위에 쌓여진 Fragement들을 버려버리고 새로운 Fragment를 쌓음
+                fragmentTransaction.replace(R.id.fragment_container_view_main, mapFragment);
+                fragmentTransaction.commit();
                 break;
 
             case 1:
-                ft.replace(R.id.Main_Frame,frag2);
-                ft.commit();
+                fragmentTransaction.replace(R.id.fragment_container_view_main, listFragment);
+                fragmentTransaction.commit();
                 break;
 
             case 2:
-                ft.replace(R.id.Main_Frame,frag3);
-                ft.setReorderingAllowed(true);
-                ft.addToBackStack(null);
-                ft.commit();
+                fragmentTransaction.replace(R.id.fragment_container_view_main, settingFragment);
+                fragmentTransaction.commit();
                 break;
+
         }
+
     }
 
+    //백그라운드 서비스 시작
+    public void service_start() {
+        startService(new Intent(this, BackgroundLocationUpdateService.class));
+    }
+
+    //백그라운드 서비스 종료
+    public void service_stop() {
+        stopService(new Intent(this, BackgroundLocationUpdateService.class));
+    }
 
     /* 뒤로가기 버튼 메소드*/
-    public void onBackPressed(){
+    public void onBackPressed() {
         //super.onBackPressed();
         //기존의 뒤로가기 버튼 기능 막기
         if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
